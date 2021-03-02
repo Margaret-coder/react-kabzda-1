34c65f90e274b4d7053242d2a9fab26b0b2cde11
Margaret <sladmorkva@gmail.com>
2021-03-02 03:35:03 +0200

    add message alert interactive

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,21 @@
 import s from './MyPosts.module.css'
+import React from 'react'
 import Post from '../Post/Post.jsx'
 
 const MyPosts = (props) => {
-
+    let newPostElement = React.createRef()
+    const addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    }
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
-                <div><button>Add post</button></div>
+                <div><button onClick={addPost}>Add post</button></div>
             </div>
             <div className={s.posts}>
                 {props.posts}
