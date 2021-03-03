525dbbb45aa9c3bc717293626da7a33c77fa9b2a
Margaret <sladmorkva@gmail.com>
2021-03-03 02:57:15 +0200

    add render.js for to avoid cyclic dependencies

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import state, { addPost, postsDataLength} from "./redux/state"
-import { BrowserRouter } from "react-router-dom";
+import rerenderEntireTree from "../src/render"
+import state from "./redux/state";
 
-addPost("post")
-ReactDOM.render(
-  <BrowserRouter>
-    <App
-    state={state}
-    addPost={addPost}
-    />
-</BrowserRouter>, document.getElementById("root")
-);
+rerenderEntireTree(state)
+
