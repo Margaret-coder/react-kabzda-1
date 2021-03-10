10bfa7087e57394133cd4b2659798a61c01e389a
Margaret <sladmorkva@gmail.com>
2021-03-10 22:19:03 +0200

    add real redux to project, add combineReducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./index.css";
-import store from "./redux/store";
+import store from "./redux/redux-store";
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 
 export let rerenderEntireTree = (state) => {
+  //debugger
     ReactDOM.render(
       <BrowserRouter>
         <App
@@ -15,7 +16,11 @@ export let rerenderEntireTree = (state) => {
 }
 
 rerenderEntireTree(store.getState())
-store.subscribe(rerenderEntireTree)
+
+store.subscribe(() => {
+  let state = store.getState()
+  rerenderEntireTree(state)
+})
 
 
 
