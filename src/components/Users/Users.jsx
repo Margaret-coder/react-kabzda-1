8548c29547ccc61048a6e86227d0ea687abebbf0
Margaret <sladmorkva@gmail.com>
2021-03-16 14:43:46 +0200

    first ajax call

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,11 +1,23 @@
+import * as axios from 'axios'
 import s from './Users.module.css'
+import userPhoto from '../../assets/images/samurai.png'
 let Users = (props) => {
+    if(props.users.length === 0){
+        let users = axios.get(
+            "https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                debugger;
+                props.setUser(response.data.items)
+            })
+    }
+
     return (
     <div>
        {props.users.map(u=> <div key = {u.id}>
            <span>
                <div>
-                   <img src={u.profilePhoto}
+                   {/* <img src={u.profilePhoto} */}
+                   <img src={u.photos.small !== null? u.photos.small : userPhoto}
                    alt="profilePhoto"/>
                </div>
                <div>
@@ -22,11 +34,11 @@ let Users = (props) => {
            </span>
            <span>
                 <span>
-                    <div>{u.fullName}</div><div>{u.status}</div>
+                    <div>{u.name}</div><div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.city}</div>
-                    <div>{u.location.country}</div>
+                    <div>{"u.location.city"}</div>
+                    <div>{"u.location.country"}</div>
                 </span>
            </span>
        </div>)
