61019465e30aa5ec2772a18a0ce09247af2b468b
Margaret <sladmorkva@gmail.com>
2021-07-31 06:00:25 +0300

    add profile info edit form checkbox behavior

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,24 @@
 import MyPostsContainer from "./MyPosts/MyPostsContainer"
 import ProfileInfo from "./ProfileInfo/ProfileInfo"
+import ProfileInfoEditMode from "./ProfileInfo/ProfileInfoEditMode"
 const Profile = (props) => {
-    console.log("Profile props", props)
-    return (
-        <div>
-            <ProfileInfo profile={props.profile}
-            status={props.status} updateStatus={props.updateStatus} authorizedUserId={props.authorizedUserId}/>
-            <MyPostsContainer/>
-        </div>
-    )
+    if (props.authorizedUserId && !props.profile) {
+        return (
+            <div>
+                <ProfileInfoEditMode profile={props.profile}
+                authorizedUserId={props.authorizedUserId}/>
+                <MyPostsContainer/>
+            </div>
+        )
+    }
+    else
+        return (
+            <div>
+                <ProfileInfo profile={props.profile}
+                status={props.status} updateStatus={props.updateStatus} authorizedUserId={props.authorizedUserId}/>
+                <MyPostsContainer/>
+            </div>
+        )
 }
 
 export default Profile
